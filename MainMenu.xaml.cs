9962823e3124e8a2fe09087621907d879aba9978
marcmis979 <72617994+marcmis979@users.noreply.github.com>
2022-07-20 16:15:04 +0200

    lets begin

---
+++ MainMenu.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fitamza
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+            lbPlany.Items.Add("Cardio");
+            lbPlany.Items.Add("Masa");
+        }
+        void lbPlany_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //wejdź w aktualny plan do wykonania
+
+        }
+
+        private void btnZacznijTrening_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
